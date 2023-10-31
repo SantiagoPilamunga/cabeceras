34f19122497f76c421eec5670c52971904d71060
Santiago <santiago.pilamunga@udla.edu.ec>
2023-10-31 15:30:29 -0500

    funcion

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "archivo1.h"
+#include "archivo2.h"
+#include "archivo3.h"
+#include "archivo4.h"
 int main()
 {
     int a=0, b=0;
@@ -7,7 +10,10 @@ int main()
     scanf("%d", &a);
     printf("Ingrese el segundo valor: ");
     scanf("%d", &b);
-    printf("El resultado de la suma es: %d", suma(a,b));
+    printf("\n El resultado de la suma es: %d \n", suma(a,b));
+    printf("\n El resultado de la resta es: %d \n", resta(a,b));
+    printf("\n El resultado de la multiplicacion es: %d \n", multiplicacion(a,b));
+    printf("\n El resultado de la division es: %d \n", division(a,b));
 
     return 0;
 }
